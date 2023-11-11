f27382606f7feefc375f5d827b48217e45a007f2
zhamandaa <zhamanda@wharton.upenn.edu>
2023-11-11 14:49:12 -0500

    added signup page and fixed initial wrong commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,7 @@
 // import './App.css';
 import Preferences from './Preferences';
+import Signup from './Signup';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
       <div className="App">
       <Routes>
           <Route path="/preferences" element={<Preferences />} />
+          <Route path="/signup" element={<Signup />} />
        </Routes>
       </div>
     </Router>
